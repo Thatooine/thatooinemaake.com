ad3cbb1ab59d228665909bce9820991b0716208a
ThatoMaake <tmthatoo4@gmail.com>
2022-06-26 03:21:50 +0200

    blinking loading

--- components/Loader/v1/index.tsx
+++ components/Loader/v1/index.tsx
@@ -1,16 +1,23 @@
-import {useSpring,animated} from "react-spring";
+import {useSpring, animated} from "react-spring";
 
 export const FancyLoader = () => {
     const props = useSpring({
         loop: true,
         to: [
-            { opacity: 1, color: 'rgb(80,80,80)' },
-            { opacity: 0, color: 'rgb(0,0,0)' },
+            {opacity: 1, color: 'rgb(80,80,80)'},
+            {opacity: 0, color: 'rgb(0,0,0)'},
         ],
-        from: { opacity: 0, color: '#16b8f3' },
+        from: {opacity: 0, color: '#16b8f3'},
     })
 
     return (
-     <animated.h1 style={props}>Loading</animated.h1>
+        <animated.div style={props}>
+            <div
+                style={{
+                    fontFamily: "Papyrus",
+                    fontSize: '12px',
+                }}
+            >Loading</div>
+        </animated.div>
     )
 }
