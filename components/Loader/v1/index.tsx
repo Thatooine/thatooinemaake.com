78bc72c3eeabfabcd21051274aeee59b6cdbfa3c
ThatoMaake <tmthatoo4@gmail.com>
2022-07-17 01:08:18 +0200

    add some timer

--- components/Loader/v1/index.tsx
+++ components/Loader/v1/index.tsx
@@ -1,53 +1,69 @@
-import {useSpring, animated, useSpringRef, Controller} from "react-spring";
+import {useSpring, animated, useSpringRef, Controller, easings, config} from "react-spring";
 import {useEffect, useRef} from "react";
 
 export const FancyLoader = () => {
-    const props = useSpring({
-        loop: true,
-        to: [
-            {opacity: 1, color: 'rgb(80,80,80)'},
-            {opacity: 0, color: 'rgb(0,0,0)'},
-        ],
-        from: {opacity: 0, color: '#16b8f3'},
+    const {number} = useSpring({
+        from: {number: 0},
+        number: 100,
+        delay: 200,
+        config: {
+            ...config.molasses,
+            duration: 2500,
+        },
     })
 
     const styles = useSpring({
         loop: true,
+        delay: 0,
+        config: {
+            ...config.molasses,
+            duration: 40,
+            damping: 0,
+        },
         to: (() => {
             const to = []
-            for(let val =  -10; val < 10; val++){
+            for (let val = -100; val <= 100; val += 5) {
                 to.push({
                     x: val,
-                    y: 2 * val * val
+                    y: Math.sqrt(100 * 100 - Math.pow(val, 2))
                 })
             }
+
+            // reverse
+            for (let val = 100; val >= -100; val -= 5) {
+                to.push({
+                    x: val,
+                    y: -1 * Math.sqrt(100 * 100 - Math.pow(val, 2))
+                })
+            }
+
             return to
         })(),
         from: {
-            x: -10,
-            y: 200
+            x: -100,
+            y: 0
         },
     })
 
     return (
-        <div>
+        <div
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: 'center'
+            }}
+        >
             <animated.div
                 style={{
-                    width: 10,
-                    height: 10,
+                    width: 20,
+                    height: 20,
                     borderRadius: '50%',
                     backgroundColor: '#46e891',
                     ...styles
                 }}
             />
-            <animated.div style={props}>
-                <div
-                    style={{
-                        fontFamily: "Papyrus",
-                        fontSize: '12px',
-                    }}
-                >Loading
-                </div>
+            <animated.div>
+                {number.to(n => n.toFixed(0))}
             </animated.div>
         </div>
     )
