31945ffe1bc05a9c6233d7a291239f8b50f94425
ThatoMaake <tmthatoo4@gmail.com>
2022-08-06 00:50:11 +0200

    explore how routing works

---
+++ pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import {NextPage} from "next";
+
+
+const Home: NextPage = () => {
+    return (
+        <div>
+            Blog page
+        </div>
+    )
+}
+
+export default Home
---
+++ pages/gallary/index.tsx
@@ -0,0 +1,12 @@
+import {NextPage} from "next";
+import exp from "constants";
+
+const Gallary: NextPage = () => {
+    return (
+        <div>
+            Gallary Page
+        </div>
+    )
+}
+
+export default Gallary
