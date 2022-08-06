ddfb3db9822b70165340a8a4e9d2854eb0c8bac7
ThatoMaake <tmthatoo4@gmail.com>
2022-08-07 01:35:49 +0200

    add mantine framework

---
+++ components/navbar/v1/index.tsx
@@ -0,0 +1,7 @@
+export const NavBar = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,44 @@
 import type {NextPage} from 'next'
-import {FancyLoader} from "../components/Loader/v1";
-import Link from "next/link";
-
+import { AppShell, Navbar, Header } from '@mantine/core';
+import {FancyLoader} from "../components/loader/v1";
 
 // 1. The fancy loader must potentially fade out after loading happens
 // 2. Slight change on the background color density (darker blue)
 // 3. Display the main landing page
 const App: NextPage = () => {
     return (
-        <div
-            style={{
-                display: 'flex',
-                height: '100vh',
-                alignItems: 'center',
-                justifyContent: "center",
-                backgroundImage: "radial-gradient(circle, rgb(49, 144, 228) 0%, rgb(29, 84, 166) 100%)"
-            }}
-        >
-            <FancyLoader/>
-        </div>
+            <AppShell
+                padding="md"
+                header={
+                <Header
+                    styles={() => ({
+                        root: {
+                            border: "none",
+                            backgroundColor: "rgb(225, 192, 58)",
+                        }
+                    })}
+                    height={60}
+                    p="xs"
+                >
+                    <div>Header Content</div>
+                </Header>}
+                styles={(theme) => ({
+                    main: {
+                        backgroundImage: "radial-gradient(circle, rgb(49, 144, 228) 0%, rgb(29, 84, 205) 193%)"
+                    },
+                })}
+            >
+                <div
+                    style={{
+                        display: 'flex',
+                        height: '100vh',
+                        alignItems: 'center',
+                        justifyContent: "center",
+                    }}
+                >
+                    <FancyLoader/>
+                </div>
+            </AppShell>
     )
 }
 
