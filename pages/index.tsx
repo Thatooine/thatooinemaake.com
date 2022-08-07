de3dbcdda7b36efaede6fde2e4c968f4caa3e7ca
ThatoMaake <tmthatoo4@gmail.com>
2022-08-07 02:14:32 +0200

    skeleton buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,76 @@
 import type {NextPage} from 'next'
-import { AppShell, Navbar, Header } from '@mantine/core';
-import {FancyLoader} from "../components/loader/v1";
+import {AppShell, Navbar, Header} from '@mantine/core';
+import {Button} from '@mantine/core';
 
 // 1. The fancy loader must potentially fade out after loading happens
 // 2. Slight change on the background color density (darker blue)
 // 3. Display the main landing page
 const App: NextPage = () => {
     return (
-            <AppShell
-                padding="md"
-                header={
+        <AppShell
+            padding="md"
+            header={
                 <Header
                     styles={() => ({
                         root: {
                             border: "none",
                             backgroundColor: "rgb(225, 192, 58)",
+                            display: "flex",
+
                         }
                     })}
                     height={60}
                     p="xs"
                 >
-                    <div>Header Content</div>
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: 'center'
+                        }}
+                    >
+                        ☀️
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "row-reverse",
+                            gap: '8px',
+                            width: "100%"
+                        }}
+                    >
+                        <Button
+                            variant={"outline"}
+                        >
+                            Resume
+                        </Button>
+                        <Button
+                            variant={"outline"}
+                        >
+                            Gallery
+                        </Button>
+                        <Button
+                            variant={"outline"}
+                        >
+                            Blog
+                        </Button>
+                    </div>
                 </Header>}
-                styles={(theme) => ({
-                    main: {
-                        backgroundImage: "radial-gradient(circle, rgb(49, 144, 228) 0%, rgb(29, 84, 205) 193%)"
-                    },
-                })}
+            styles={(theme) => ({
+                main: {
+                    backgroundImage: "radial-gradient(circle, rgb(49, 144, 228) 0%, rgb(29, 84, 205) 193%)"
+                },
+            })}
+        >
+            <div
+                style={{
+                    display: 'flex',
+                    height: '100vh',
+                    alignItems: 'center',
+                    justifyContent: "center",
+                }}
             >
-                <div
-                    style={{
-                        display: 'flex',
-                        height: '100vh',
-                        alignItems: 'center',
-                        justifyContent: "center",
-                    }}
-                >
-                    <FancyLoader/>
-                </div>
-            </AppShell>
+            </div>
+        </AppShell>
     )
 }
 
